d26c35275ab2745e7a97352b7737706bce0a526e
Alfredoooh <alfredopjonas@gmail.com>
2026-07-09 20:56:11 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        app: resolve(__dirname, 'src/minha-app/index.html'),
+        notfound: resolve(__dirname, 'src/notfound/index.html'),
+      }
+    }
+  }
 });

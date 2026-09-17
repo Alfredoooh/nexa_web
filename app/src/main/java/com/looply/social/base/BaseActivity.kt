17847a9f809850dc68d559d424cabdbb353909c8
Alfredoooh <alfredopjonas@gmail.com>
2026-09-17 20:34:01 +0100

    update

--- app/src/main/java/com/looply/social/base/BaseActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.looply.social.base
-
-import androidx.appcompat.app.AppCompatActivity
-
-abstract class BaseActivity : AppCompatActivity() {
-
-    // Funções e comportamentos comuns a todas as activities entram aqui
-}
